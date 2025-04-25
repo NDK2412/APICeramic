0bb64161016d8dd091b0b49ee50d59f16b22ac13
Tên của bạn <Email ca bn>
2025-04-25 14:13:55 +0700

    25/4

--- htdocs/Ceramic_Detection/Ceramic_Detection/resources/views/result.blade.php
+++
@@ -1,4 +0,0 @@
-<h2>Kết quả dự đoán:</h2>
-<p>Loại gốm sứ: {{ $result['predicted_class'] }}</p>
-<p>Độ tin cậy: {{ $result['confidence'] * 100 }}%</p>
-<p>Thông tin chi tiết: {{ $result['llm_response'] }}</p>
